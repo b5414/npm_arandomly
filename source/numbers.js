35d7b2ca213195b0e201dcd6f0049e0a1b0e5b40
b5414 <mr.b5414@yandex.ru>
2020-10-13 16:42:17 +0800

    4

--- source/numbers.js
+++ source/numbers.js
@@ -5,11 +5,39 @@ const {minMaxCheck} = require('./globals.js');
 //
 
 /**
+ * @brief Random bool
+ *
+ * @return {boolean} - true or false
+ */
+const randBool = ()=>!rand(0, 1);
+
+/**
+ * @brief Random bool
+ *
+ * @param {integer} [True_Chance=50] - Percent chance to get true (number)
+ * @return {boolean} true or false
+ */
+const bool = (trueChance = 50)=>{
+	if(!trueChance)trueChance = 50;
+	else if(trueChance < 0)trueChance = 0;
+	trueChance = parseInt(trueChance);
+
+	if(trueChance === 0)return false;
+	else if(trueChance === 100)return true;
+	else if(trueChance !== 50)return rand(0, 100) <= trueChance;
+	return randBool();
+};
+
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+
+/**
  * @brief Random int from A to B
  *
  * @param {number} [min=0] - Minimum (number, inclusive)
  * @param {number} [max=100] - Maximum (number, inclusive)
- * @return {number}
+ * @return {number} - Number between
  */
 const rand = (min = 0, max = 100)=>{
 	[min, max] = minMaxCheck(min, max);
@@ -40,50 +68,17 @@ const random = (min, max)=>rand(min, max) / 100;
 const randFloat = random;
 
 /**
- * @brief Random float from A to B, with C symbols after point
+ * @brief Random float from 0.AA to 0.BB, with C symbols after point
  *
- * @param {number} [min=0] - Minimum (number, inclusive)
- * @param {number} [max=100] - Maximum (number, inclusive)
+ * @param {number} [min=0] - Minimum .A (number, inclusive)
+ * @param {number} [max=100] - Maximum .B (number, inclusive)
  * @param {number} [decimal=2] - Symbols after dot (number)
- * @return {number} - 0.0(n*0) to 10*n
+ * @return {number} 0 to 1 (or to more than 1), with N numbers after dot
  */
-const randBigFloat = (min, max, decimal = 2)=>{
-	Math.random();
-};
-
-// console.log(randFloat(1, 100, 10));
-
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-
-/**
- * @brief Random bool
- *
- * @return {boolean} - true or false
- */
-const randBool = ()=>!rand(0, 1);
-
-/**
- * @brief Random bool
- *
- * @param {integer} [min=0] - Chance to recieve true (number)
- * @return {boolean} - true or false
- */
-const bool = (trueChance = 50)=>{
-	if(trueChance < 0){
-		trueChance = 0;
-	}
-	trueChance = parseInt(trueChance);
-
-	if(trueChance !== 50){
-		return random(0, 100) <= trueChance / 100;
-	}else if(trueChance === 0){
-		return false;
-	}else if(trueChance === 100){
-		return true;
-	}
-	return randBool();
+const randBigFloat = (min = 0, max = 100, decimal = 3)=>{
+	let r = (min + Math.random() * (max + 1 - min)) / 100;
+	const b = `${r}`.indexOf('.');
+	return +`${r}`.slice(0, b + decimal + 1);
 };
 
 //

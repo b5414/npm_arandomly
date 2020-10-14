32c56c19e16657457beacaeea3ddd65aa7c909c1
b5414 <mr.b5414@yandex.ru>
2020-10-14 14:36:26 +0800

    7

--- index.js
+++ index.js
@@ -5,30 +5,21 @@ const {bool, randBool, rand, randInt, random, randFloat, randBigFloat} = require
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+/**
+ * @description Roll random number 1 to 100
+ *
+ * @param {boolean} [detailed=false] Detailed object output
+ * @returns {number | object} number 1-100 | obj {result, text, pad, padSpace}
+ */
+const roll = (detailed = false)=>{
+	const result = rand(1, 100);
+	if(detailed){
+		const a = padLeft(result, 3);
+		const b = dilute(a);
+		return {text: `rolls (1-100): ${b}`, result, pad: a, padSpace: b};
+	}
+	return result;
+};
 
 /**
  * @description Random element from array
@@ -42,6 +33,28 @@ const oneOfArray = (arr)=>arr[rand(0, arr.length - 1)];
  * @alias oneOfArray
  */
 const randomElement = oneOfArray;
+
+/**
+ * @description Random string from A-Z & a-z letters
+ *
+ * @param {number} [leng=4] Length of output
+ * @param {string} [symbols="A-z alphabet"] Pool of letters\symbols\numbers
+ * @returns {string} Random string
+ */
+const randomString = (leng = 4, symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')=>{
+	let result = '';
+	for(let k = 0; k < leng; k++)result = `${result}${symbols.charAt(Math.floor(Math.random() * symbols.length))}`;
+	return result;
+};
+
+/**
+ * @description Random date from 1970 to 2038 (Yes, unix)
+ *
+ * @param {number} [min=0] - Minimum unix secs (number)
+ * @param {number} [max=2147483648] - Maximum unix secs (number)
+ * @returns {date} Random Date
+ */
+const randomDate = (min = 0, max = 2147483648)=>new Date(rand(min, max)*1000);
 
 //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -74,22 +87,9 @@ const randomDateYear = (year = [])=>{
 	return date;
 };
 
-const roll = (detailed = false)=>{
-	const result = rand(1, 100);
-	if(detailed){
-		const a = padLeft(result, 3);
-		const b = dilute(a);
-		return {text: `rolls (1-100): ${b}`, result, pad: a, padSpace: b};
-	}
-	return result;
-};
-
-const randomString = (leng = 9)=>{
-	const astring = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	
-	// float to 123123, take 
-	return astring;
-};
+console.log(randomDate());
+// console.log(randomDate());
+// console.log(randomDateYear(2017));
 
 const randomArrayGen = (leng, obj = {})=>{
 	if(!leng || typeof leng !== 'number' || typeof obj !== 'object' || Array.isArray(obj))return false;
@@ -108,9 +108,9 @@ const randomArrayGen = (leng, obj = {})=>{
 	// return rand(min, max) / 100;
 };
 
-console.log(randomArrayGen(2, {}));
-console.log(randomArrayGen(3, []));
-console.log(randomArrayGen(0, []));
+// console.log(randomArrayGen(2, {}));
+// console.log(randomArrayGen(3, []));
+// console.log(randomArrayGen(0, []));
 
 //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -118,21 +118,24 @@ console.log(randomArrayGen(0, []));
 
 module.exports = {
 	/* index */
+	roll,
 	oneOfArray,
 	randomElement,
-	/* ~~~~~ */
+	randomString,
+	randomDate,
+	randomDateYear,
+	randomArrayGen,
+
 	/* globals */
 	padLeft,
 	dilute,
 	minMax,
-	/* ~~~~~ */
-	/* bool */
+
+	/* numbers */
 	bool,
 	randBool,
-	/* int */
 	rand,
 	randInt,
-	/* float */
 	random,
 	randFloat,
 	randBigFloat,

4595a1aadccab6110b53a3dab161aab6f16584d6
b5414 <mr.b5414@yandex.ru>
2020-10-15 10:36:07 +0800

    10
    randBigFloat

--- source/numbers.js
+++ source/numbers.js
@@ -5,25 +5,25 @@ const {minMax} = require('./globals.js');
 //
 
 /**
- * @brief Random bool
+ * @description Random bool
  *
- * @return {boolean} - true or false
+ * @return {boolean} True or False
  */
 const randBool = ()=>!rand(0, 1);
 
 /**
- * @brief Random bool
+ * @description Random bool, with chance param
  *
- * @param {integer} [True_Chance=50] - Percent chance to get true (number)
- * @return {boolean} true or false
+ * @param {number} [trueChance=50] - Percentage chance of True
+ * @return {boolean} True or False
  */
 const bool = (trueChance = 50)=>{
 	if(!trueChance)trueChance = 50;
 	else if(trueChance < 0)trueChance = 0;
 	trueChance = parseInt(trueChance);
 
-	if(trueChance === 0)return false;
-	else if(trueChance === 100)return true;
+	if(trueChance <= 0)return false;
+	else if(trueChance >= 100)return true;
 	else if(trueChance !== 50)return rand(0, 100) <= trueChance;
 	return randBool();
 };
@@ -33,11 +33,11 @@ const bool = (trueChance = 50)=>{
 //
 
 /**
- * @brief Random int from A to B
+ * @description Random number from MIN to MAX (both inclusive)
  *
- * @param {number} [min=0] - Minimum (number, inclusive)
- * @param {number} [max=100] - Maximum (number, inclusive)
- * @return {number} - Number between
+ * @param {number} [min=0] Minimum
+ * @param {number} [max=100] Maximum
+ * @return {number} Random number (or float)
  */
 const rand = (min = 0, max = 100)=>{
 	[min, max] = minMax(min, max);
@@ -54,11 +54,12 @@ const randInt = rand;
 //
 
 /**
- * @brief Random float from A to B
+ * @description Random float from MIN to MAX (both inclusive)
+ * @summary Same as ".rand()", but "/ 100"
  *
- * @param {number} [min=0] - Minimum (number, inclusive)
- * @param {number} [max=100] - Maximum (number, inclusive)
- * @return {number} - 0.00 to 1.00
+ * @param {number} [min=0] Minimum
+ * @param {number} [max=100] Maximum
+ * @return {number} Random float (or number)
  */
 const random = (min, max)=>rand(min, max) / 100;
 
@@ -68,18 +69,78 @@ const random = (min, max)=>rand(min, max) / 100;
 const randFloat = random;
 
 /**
- * @brief Random float from 0.AA to 0.BB, with C symbols after point
+ * @description Random float from MIN to MAX (both inclusive), with N Symbols after point
  *
- * @param {number} [min=0] - Minimum .A (number, inclusive)
- * @param {number} [max=100] - Maximum .B (number, inclusive)
- * @param {number} [decimal=2] - Symbols after dot (number)
- * @return {number} 0 to 1 (or to more than 1), with N numbers after dot
+ * @param {number} [min=0] - Minimum
+ * @param {number} [max=100] - Maximum
+ * @param {number} [decimal=3] - Symbols after dot (1-20)
+ * @return {number} Random float, with N numbers after dot
  */
 const randBigFloat = (min = 0, max = 100, decimal = 3)=>{
-	let r = (min + Math.random() * (max + 1 - min)) / 100;
-	const b = `${r}`.indexOf('.');
-	return +`${r}`.slice(0, b + decimal + 1);
+	[min, max] = minMax(min, max);
+
+	// let r = min + Math.random() * (max - min + 1);
+	let r = Math.random() * (max - min) + min;
+	// let r = max - min + 1;
+	// console.log(r);
+	// r = Math.random() * r;
+	// console.log(r);
+	// r = min + r;
+
+	// if(bool(1))r = max;
+	// else if(bool(1))r = min;
+	return (r / 100).toFixed(decimal);
 };
+
+// let tf = true;
+// for(let k = 0; tf; k++){
+// 	let r = randBigFloat(0, 2, 16);
+// 	console.log(r);
+// 	// if(r === 0 || r > 1){
+// 	if(k > 2){
+// 		tf = false;
+// 	}
+// }
+
+const aaaflaot = (min, max, decimal = 4)=>{
+	let num = Math.pow(10, decimal);
+	console.log(num);
+	let r = rand(min * num, max * num);
+	r = r / num;
+	return (r / 100).toFixed(decimal);
+};
+
+const aaaflaot2 = (min, max, decimal = 4)=>{
+	let r = 0;
+	if(bool(1) && bool(1) && bool(1) && bool(1)){
+		r = randBool() ? max : min;
+	}else{
+		const mpd = Math.pow(10, decimal);
+		r = rand(min * mpd, max * mpd) / mpd;
+	}
+	return (r / 100).toFixed(decimal);
+};
+
+// let tf = true;
+// for(let k = 0; tf; k++){
+// 	let r = aaaflaot(0, 100, 10);
+// 	// if(r < 0.00000001 || r >= 1){
+// 	if(r >= 1){
+// 		console.log(r);
+// 		tf = false;
+// 	}
+// }
+
+let arr = [];
+for(let k = 0; k < 200; k++){
+	let r = aaaflaot(90, 100);
+	if(r > 0.9015 && r < 0.9995)k--;
+	else arr.push(r);
+}
+arr.sort();
+arr.forEach((e, i)=>{
+	console.log(i, e);
+});
 
 //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

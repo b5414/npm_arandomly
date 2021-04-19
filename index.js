d2475a9f1a5e71a50d02e25fa76e9b9322934dba
b5414 <mr.b5414@yandex.ru>
2021-04-20 01:28:58 +0800

    57 - Cleanup

--- index.js
+++ index.js
@@ -1,5 +1,112 @@
-const {padLeft, dilute, minMax} = require('./source/globals.js');
-const {bool, randBool, rand, randInt, random, randFloat, randBigFloat} = require('./source/numbers.js');
+/**
+ * @description "A" to "BBA"
+ *
+ * @param {string | number} n - Value to padding
+ * @param {number} [len=3] - Number of pads
+ * @param {string | number} [pad=0] - Pad ("B" - value)
+ * @returns {string} Padded string
+ */
+const padLeft = (n, len = 3, pad = 0)=>{
+	if(`${n}`.length < len){
+		n = `${pad}${n}`;
+		return padLeft(n, len, pad);
+	}
+	return n;
+};
+
+/**
+ * @description "AAA" to "ABABA" (explode + implode)
+ *
+ * @param {string | number} value - Value to dilute
+ * @param {string | number} [glue=' '] - Implode glue ("B" - value)
+ * @returns {string}
+ */
+const dilute = (value, glue = ' ')=>`${value}`.split('').join(`${glue}`);
+
+/**
+ * @description A and B to [A, B] or [B, A]
+ *
+ * @param {number} any Number
+ * @param {number} any Number
+ * @returns {array} array [min, max] of numbers
+ */
+const minMax = (min, max)=>{
+	if(min > max)[min, max] = [max, min];
+	return [min, max];
+};
+
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+
+/**
+ * @description Random bool value
+ *
+ * @return {boolean} True or False
+ */
+const randBool = ()=>!rand(0, 1);
+
+/**
+ * @description Random bool, with true chance percent
+ *
+ * @param {number} [trueChance=50] - Percentage chance to True
+ * @return {boolean} True or False
+ */
+const bool = (trueChance = 50)=>{
+	if(!trueChance && trueChance !== 0)trueChance = 50;
+	else trueChance = parseInt(trueChance);
+
+	if(trueChance <= 0)return false;
+	else if(trueChance >= 100)return true;
+	else if(trueChance !== 50)return rand(0, 100) <= trueChance;
+	return randBool();
+};
+
+/**
+ * @description Random number from MIN to MAX (both inclusive)
+ *
+ * @param {number} [min=0] Minimum value
+ * @param {number} [max=100] Maximum value
+ * @return {number} Random value
+ */
+const rand = (min = 0, max = 100)=>{
+	[min, max] = minMax(min, max);
+	return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+/**
+ * @alias rand
+ */
+const randInt = rand;
+
+/**
+ * @description Random number from MIN to MAX (both inclusive)
+ * @summary Same as ".rand()", but +"/ 100"
+ *
+ * @param {number} [min=0] Minimum value
+ * @param {number} [max=100] Maximum value
+ * @return {number} Random number
+ */
+const random = (min, max)=>rand(min, max) / 100;
+
+/**
+ * @alias random
+ */
+const randFloat = random;
+
+/**
+ * @description Random float from MIN to MAX (both inclusive), with N Symbols after point
+ *
+ * @param {number} [min=0] - Minimum value
+ * @param {number} [max=100] - Maximum value
+ * @param {number} [decimal=3] - Number of decimal places (0-18)
+ * @return {number} Random float, with N numbers after dot
+ */
+const randBigFloat = (min = 0, max = 100, decimal = 3)=>{
+	[min, max] = minMax(min, max);
+	const mpd = Math.pow(10, decimal);
+	return parseFloat((rand(min * mpd, max * mpd) / mpd / 100).toFixed(decimal));
+};
 
 //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
